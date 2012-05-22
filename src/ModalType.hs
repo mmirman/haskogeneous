fd2597eb9ccbcad83cf697618aee98ff4abbd54d
Matthew Mirman <mmirman@andrew.cmu.edu>
2012-05-21 23:48:04 -0400

    added nu reductions

--- src/ModalType.hs
+++ src/ModalType.hs
@@ -12,8 +12,8 @@
 
 module ModalType where
 
-import Control.Monad.Writer.Strict (Writer, tell, execWriter)
-import Control.Monad.Error
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Class (lift)
 import Data.Foldable hiding (foldr)
 import Data.Monoid
 import Data.Functor
@@ -21,7 +21,13 @@ import Data.Map as M
 import Data.Set as S
 import Debug.Trace
 
-type TpVar = String
+import Data.Unique
+
+data Var = Lit String 
+         | Gen Int
+         deriving (Eq, Ord, Show, Read)
+
+type TpVar = Var
 type TmVar = String
 type Loc = String
 type World = S.Set String
@@ -39,17 +45,17 @@ data Tm = Lam Tp TmVar Tm
         | BoxAt World Tm
         | TmVar TmVar
         | Const
-          
         | TyLam TpVar Tm          
-        | TyApp Tm Tp          
+        | TyApp Tm Tp
         deriving (Eq, Show, Read)
 
-typeCheck :: Tm -> Maybe Tp
+
+typeCheck :: Tm -> MaybeT IO Tp
 typeCheck = generate mempty mempty
 
 substitute :: TpVar -> Tp -> Tp -> Tp
 substitute tO tN t = case t of
-  Forall tO' e | tO' == tO -> t
+  Forall tO' _ | tO' == tO -> t
   Forall tO' e  -> Forall tO' $ substitute tO tN e
   TpVar tO' | tO' == tO -> tN
   TpVar tO' -> TpVar tO'
@@ -57,7 +63,21 @@ substitute tO tN t = case t of
   t1 :-> t2 -> substitute tO tN t1 :-> substitute tO tN t2
   t1 :@ w -> substitute tO tN t1 :@ w
 
-generate :: M.Map TmVar Tp -> M.Map Loc (M.Map TmVar Tp) -> Tm -> Maybe Tp
+
+regen :: Tp -> MaybeT IO Tp
+regen (Forall tO t) = do
+  u <- Gen <$> hashUnique <$> lift newUnique 
+  return $ Forall u $ substitute tO (TpVar u) t
+regen (t :@ w) = (:@ w) <$> regen t
+regen (t1 :-> t2) = do
+  t1' <- regen t1 
+  t2' <- regen t2
+  return $ t1' :-> t2'
+regen t = return t
+
+returnMaybe = MaybeT . return
+
+generate :: M.Map TmVar Tp -> M.Map Loc (M.Map TmVar Tp) -> Tm -> MaybeT IO Tp
 generate mpLocal mpGlobal l = case l of 
   Lam a tv e -> do
     b <- generate (M.insert tv a mpLocal) (fmap (M.delete tv) mpGlobal) e
@@ -71,13 +91,13 @@ generate mpLocal mpGlobal l = case l of
     where local = foldr (M.intersection . get) (get a) (a:l)
           get = maybe mempty id . (`M.lookup` mpGlobal)
           (a:l) = (S.toList world)
-  TmVar tv -> M.lookup tv mpLocal 
+  TmVar tv -> returnMaybe $ M.lookup tv mpLocal 
   Const -> return Unit
   App e1 e2 -> do
     let gen = generate mpLocal mpGlobal 
     (a :-> b) <- gen e1 
     a' <- gen e2 
-    if a == a' then Just b else Nothing
+    returnMaybe $ if a == a' then Just b else Nothing
     
   TyLam ty e -> do
     b <- generate mpLocal mpGlobal e
@@ -85,7 +105,5 @@ generate mpLocal mpGlobal l = case l of
   
   TyApp e ty -> do
     Forall tv b <- generate mpLocal mpGlobal e
-    -- ASSUME NO ETA: THIS WILL BREAK!
-    return $ substitute tv ty b
-    
-    
+    ty' <- regen ty
+    return $ substitute tv ty' b

3d58cf1da267c91c97827f9531605fcb8db9d1f6
Matthew Mirman <mmirman@palantir.com>
2012-07-11 13:22:47 -0700

    monadified check in ModalHaskell

--- ModalHaskell.hs
+++ ModalHaskell.hs
@@ -2,12 +2,15 @@
  FlexibleInstances,
  UndecidableInstances,
  GeneralizedNewtypeDeriving,
- ScopedTypeVariables
+ ScopedTypeVariables,
+ ViewPatterns
  #-}
 module ModalHaskell (Box(..), build, toServer, Data(..), Client(..)) where
 
 import Language.Haskell.TH
 import Data.Monoid
+import Data.Functor 
+import Control.Monad.Reader
 import qualified Data.Map as M
 
 data Box a = Box { unbox :: a }
@@ -35,28 +38,50 @@ toServer a (Box f) = LiftIO (f a)
 build :: Q [Dec] -> Q [Dec]
 build ml = do
   d <- ml
-  mapM (check 0 mempty) d
+  runReaderT (mapM check d) (0,mempty)
 
 type BoxLevel = Integer
-
 type LocalContext = M.Map Name BoxLevel
+type Context = (BoxLevel, LocalContext)
+type QCheck = ReaderT Context Q
+
+getBoxLevel :: QCheck BoxLevel
+getBoxLevel = fst <$> ask
+
+getCtxt :: QCheck LocalContext
+getCtxt = snd <$> ask
 
 class Check a where
-  check :: BoxLevel -> LocalContext -> a -> Q a
+  check :: a -> QCheck a
 
 instance Check Dec where
-  check boxlevel ctxt a = case a of
+  check a = case a of
     FunD nm _ -> undefined
     ValD pat bd decs -> undefined
     a -> return a
 
+instance Check Exp where
+  check a =
+    case a of
+      VarE nm -> VarE <$> check nm
+      AppE (ConE (nameBase -> "Box")) _ -> undefined -- Watch out below!
+      AppE a b -> do
+        a' <- check a
+        b' <- check b
+        return $ AppE a' b'
+      ConE nm -> return $ ConE nm
+      ParensE e -> ParensE <$> check e
+
 instance Check Name where
-  check boxlevel ctxt nm = case M.lookup nm ctxt of
-    Just i | i <= boxlevel -> do
-      loc <- location
-      error $ "nm: " ++ show nm 
-        ++ " @ " ++ show (loc_start loc)
-        ++ " in "++ loc_filename loc
-        ++ "\n Don't you wish this had better error reporting?" 
-        ++ "\nWhy doesn't template haskell include statistics with the expressions?"
-    _ -> return nm
+  check nm = do
+    ctxt <- getCtxt
+    boxlevel <- getBoxLevel
+    case M.lookup nm ctxt of
+      Just i | i <= boxlevel -> do
+        loc <- lift location
+        error $ "nm: " ++ show nm 
+          ++ " @ " ++ show (loc_start loc)
+          ++ " in "++ loc_filename loc
+          ++ "\n Don't you wish this had better error reporting?" 
+          ++ "\nWhy doesn't template haskell include statistics with the expressions?"
+      _ -> return nm

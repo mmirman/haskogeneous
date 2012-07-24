417c2a4bbb373c036c5df6538329880411f3c437
Matthew Mirman <mmirman@palantir.com>
2012-07-24 14:45:30 -0700

    moved everything to rpc-framework

--- src/Network/Remote/RPC.hs
+++
@@ -1,4 +0,0 @@
-module Network.Remote.RPC (module R) where
-
-import Network.Remote.RPCInternal as R (world, liftIO, Host(..), runServerBG, runServer) 
-import Network.Remote.MobileTransform as R (rpcCall, makeServices, makeHost) 
